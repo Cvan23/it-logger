9bb6c885da9293bb18618d266f701ff22440bcc6
Chris Van Huffe <cvanhuffel@outlook.com>
2021-01-25 19:04:04 -0800

    First Commit, json server set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+// import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Intitializes Materialize JS functionality
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
     </div>
   );
 }
